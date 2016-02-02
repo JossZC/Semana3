819ddde99eef71ca08a28b3960dfaa01e19c6d7e
JossZC <jocselinz@hotmail.com>
2016-02-01 19:20:24 -0600

    Version1.0

---
+++ Semana3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Boolean content = true;
+            int i = 35;
+            string comentario = "Hello World";
+            String[] myints = { "5", "10", "15" };
+            string num = "";
+            textBox1.Text = Convert.ToString(content);
+            textBox2.Text = Convert.ToString(i);
+            textBox3.Text = comentario;
+            foreach ( string Numeros in myints)
+            {
+                num = num + Numeros + " ";   
+                
+            }
+            MessageBox.Show(num);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
